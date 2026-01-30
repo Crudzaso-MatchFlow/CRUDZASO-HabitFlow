873072cbeed789736aaef763e69d5a62058a95db
miguelrodrica <mangelrodca@gmail.com>
2026-01-30 17:36:04 -0500

    Funcionalidad JavaScript del formulario crear ofertas

--- createOffer.js
+++ createOffer.js
@@ -1,11 +1,40 @@
-const form = document.querySelector('#formCreateOffer')
-const titleOffer = document.querySelector('#titleOffer')
-const descriptionOffer = document.querySelector('#descriptionOffer')
-const professionOffer = document.querySelector('#professionOffer')
-const typeContract = document.querySelector('#typeContract')
-const locationOffer = document.querySelector('#locationOffer')
-const modeOffer = document.querySelector('#modeOffer')
-const salaryOffer = document.querySelector('#salaryOffer')
-const deadlineOffer = document.querySelector('#deadlineOffer')
-const submitBtn = document.querySelector('#submitBtn')
-const cancelBtn = document.querySelector('#cancelBtn')
+let form = document.getElementById('formCreateOffer');
+const titleOffer = document.getElementById('titleOffer');
+const descriptionOffer = document.getElementById('descriptionOffer');
+const professionOffer = document.getElementById('professionOffer');
+const typeContract = document.getElementById('typeContract');
+const locationOffer = document.getElementById('locationOffer');
+const modeOffer = document.getElementById('modeOffer');
+const salaryOffer = document.getElementById('salaryOffer');
+const deadlineOffer = document.getElementById('deadlineOffer');
+let submitBtn = document.getElementById('submitBtn');
+let cancelBtn = document.getElementById('cancelBtn');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const newOffer = {
+        companyId: "",
+        title: titleOffer.value,
+        description: descriptionOffer.value,
+        profession: professionOffer.value,
+        typeContract: typeContract.value,
+        location: locationOffer.value,
+        mode: modeOffer.value,
+        salary: salaryOffer.value,
+        deadline: deadlineOffer.value,
+        createdAt: new Date(),
+        isActive: true
+    }
+
+    await fetch('http://localhost:3000/jobOffers', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newOffer)
+    });
+
+    alert('Oferta registrada correctamente');
+    form.reset();
+});
